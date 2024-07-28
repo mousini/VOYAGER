2ce2a3aae5badb6184d4d4c09c6fd00196aa0ab3
Mousini Sarkar <160837080+mousini@users.noreply.github.com>
2024-07-28 22:36:10 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+document.getElementById('menuButton').addEventListener('click', function() {
+    var menu = document.getElementById('dropdownMenu');
+    if (menu.style.display === 'block') {
+        menu.style.display = 'none';
+    } else {
+        menu.style.display = 'block';
+    }
+});
